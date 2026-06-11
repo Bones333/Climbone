8adf3dfa87c74c0c3c32b415f29f0fe7386f17d6
Bones333 <liamavital9@gmail.com>
2026-06-12 01:31:35 +0300

    Add artwork data file

---
+++ js/data.js
@@ -0,0 +1,27 @@
+// Art gallery data
+const artworks = [
+    {
+        id: 1,
+        title: "Silent Growth",
+        year: 2026,
+        price: "₪1,200",
+        image: "art1.jpg",
+        status: "available"
+    },
+    {
+        id: 2,
+        title: "Fragments",
+        year: 2025,
+        price: "₪850",
+        image: "art2.jpg",
+        status: "sold"
+    },
+    {
+        id: 3,
+        title: "Memory",
+        year: 2024,
+        price: "Not For Sale",
+        image: "art3.jpg",
+        status: "nfs"
+    }
+];
